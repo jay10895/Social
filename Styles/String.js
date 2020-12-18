9534590805add3c3f7cc9b767279e90904e6e398
jay10895 <72381275+jay10895@users.noreply.github.com>
2020-12-18 17:29:06 +0530

    Adding Styles\String.js

---
+++ Styles/String.js
@@ -0,0 +1,40 @@
+
+const string = {
+    onboarding:{
+        all:"This feild is required",
+        emailError:"Please enter email",
+        password:"Please Enter Password",
+        cnf_password:"Please Enter Confirm Password",
+        cityError:"please enter city name",
+        countryError:"please enter your country",
+        validUser:"Please enter valid username",
+        validPass:"Please enter valid password",
+        validEmail:"Please enter valid email id",
+        validForgotEmail:"Please enter valid email id",
+        validPassword:"Your password must contain atleast one special characters, one digit, one small characters, and one Capital alphabet",
+        password_mismatch:"Your password does not match.",
+        validFullName:"Please enter valid full name",
+        Validusername:"Please enter valid Username",
+        taglineError:"Please enter tag line",
+        checkError:"Please accept terms & conditions.",
+        
+        oldPasswordError:"Please enter old password",
+        
+        new_passwordError:"Please enter new password",
+        
+        cnf_passwordError:"Please enter Confirm Password",
+        
+        confirm_passwordError:"Please enter Confirm Password",
+        
+        valid_Cnf_Password:"Your password must contain atleast one special characters, one digit, one small characters, and one Capital alphabet",
+
+        fullnameError:"Please enter full name",
+        
+        userError:"please enter user name",
+        valid_Confirm_Pass:"Please enter valid Confirmpassword",
+      
+        
+    }
+}
+
+export {string}
